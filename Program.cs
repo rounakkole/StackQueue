83ecb09903df06e643e20b96ebb9a1bc52cce596
rounak <rounakkole@gmail.com>
2022-02-27 14:06:30 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace StackQueue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Stacks and Queues");
+        }
+    }
+}
